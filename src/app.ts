554aae9d50bbcb19f87cb86fde7007f253bbbf9c
Ali Madooei <alimadooei@gmail.com>
2025-05-22 16:04:00 -0400

    feat: enhance VS Code configuration for development
    - Added .vscode/extensions.json to recommend essential extensions for the project.
    - Introduced .vscode/launch.json for debugging TypeScript applications.
    - Created .vscode/settings.json to define editor preferences and settings.
    - Updated README.md with hints and instructions for using the VS Code debugger.
    - Added docs/vscode.md to provide detailed guidance on VS Code setup and usage.

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,6 @@ import { Hono } from "hono";
 export const app = new Hono();
 
 app.get("/", (c) => {
+  console.log("Hello Hono!"); // Let's stop here to test the debugger (add a breakpoint here, and run the debugger)
   return c.text("Hello Hono!");
 });
